1f5c338a2dbe5156e51acc1943e2b01c915411f6
Ketus <bardzo.mlody@gmail.com>
2015-10-16 20:54:37 +0000

    Complete HTTP JSON API SERVER

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-var http = require("http");
-var map = require("through2-map");
+var http = require('http');
+    var url = require('url');
 
-var server = http.createServer(function(req, res){
-  if (req.method === 'POST'){
-    var toUpper = req.pipe(map(function(chunk) {
-      return chunk.toString().toUpperCase();
-    }));
-  }
-  toUpper.pipe(res);
-});
+    function parsetime (time) {
+      return {
+        hour: time.getHours(),
+        minute: time.getMinutes(),
+        second: time.getSeconds()
+      };
+    }
 
-server.listen(process.argv[2]);
+    function unixtime (time) {
+      return { unixtime : time.getTime() };
+    }
 
+    var server = http.createServer(function (req, res) {
+      var parsedUrl = url.parse(req.url, true);
+      var time = new Date(parsedUrl.query.iso);
+      var result;
+
+      if (/^\/api\/parsetime/.test(req.url))
+        result = parsetime(time);
+      else if (/^\/api\/unixtime/.test(req.url))
+        result = unixtime(time);
+
+      if (result) {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(result));
+      } else {
+        res.writeHead(404);
+        res.end();
+      }
+    })
+    server.listen(Number(process.argv[2]));
